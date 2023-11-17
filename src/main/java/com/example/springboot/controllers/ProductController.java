862b6c1ef3d6099bcf2e06b2445e49e12741695f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-17 00:36:17 -0300

    [:sparkles:] | create: controller - get > product (api)

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -6,25 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-@RestController
+@RestController("products")
 public class ProductController {
 
     @Autowired
     ProductRepository repository;
 
-    @GetMapping("/products")
-    public ResponseEntity<List<ProductModel>> getAllProducts() {
+    @GetMapping("/")
+    public ResponseEntity<List<ProductModel>> getAll() {
         List<ProductModel> products = repository.findAll();
 
         return ResponseEntity.status(HttpStatus.OK).body(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOne(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> productO = repository.findById(id);
+
+        if (productO.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product Not Found");
+
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
     }
 
 }
