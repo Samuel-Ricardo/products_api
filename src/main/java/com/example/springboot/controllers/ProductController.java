72b9c67269ebde38d0e6c9699f173ba201e8effe
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-17 00:13:16 -0300

    [:sparkles:] | create: controller - get > products (api)

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -1,13 +1,28 @@
 package com.example.springboot.controllers;
 
+import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class ProductController {
 
     @Autowired
     ProductRepository repository;
+
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        List<ProductModel> products = repository.findAll();
+
+        return ResponseEntity.status(HttpStatus.OK).body(products);
+    }
 
 }
