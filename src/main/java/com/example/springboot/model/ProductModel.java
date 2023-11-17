95f8e35183ffc3dd1407b47111912806e2cb56cc
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-16 22:48:47 -0300

    [:sparkles:] | create: model - product (app)

---
+++ src/main/java/com/example/springboot/model/ProductModel.java
@@ -0,0 +1,20 @@
+package com.example.springboot.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class ProductModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+}
